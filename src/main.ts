e93a7b9c4e209e2ced2bd48314b39584b0257d56
grozdanovgg <grozdanovgg@gmail.com>
2017-11-04 17:57:38 +0200

    Enable prod mode when not local.

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
+// if (environment.production) {
+// 	console.log('Enabling production mode with enableProMode() function');
+// 	enableProdMode();
+// }
+
+// Enable production mode unless running locally
+if (!/localhost/.test(document.location.host)) {
+	console.log('Enabling production mode with enableProMode() function');
+	enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
