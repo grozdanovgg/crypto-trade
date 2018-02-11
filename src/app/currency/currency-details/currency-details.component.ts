bae2c72596da034e24470137bfca95581cc126bd
grozdanovgg <grozdanovgg@gmail.com>
2018-02-12 01:07:14 +0200

    add particular model types

--- src/app/currency/currency-details/currency-details.component.ts
+++ src/app/currency/currency-details/currency-details.component.ts
@@ -12,117 +12,117 @@ import { ActivatedRoute } from '@angular/router';
 import { OCLHRawDdata } from '../../models/OCLHdata';
 
 @Component({
-	selector: 'app-currency-details',
-	templateUrl: './currency-details.component.html',
-	styleUrls: ['./currency-details.component.css']
+    selector: 'app-currency-details',
+    templateUrl: './currency-details.component.html',
+    styleUrls: ['./currency-details.component.css']
 })
 export class CurrencyDetailsComponent implements OnInit {
-	private loggedUser: User;
-	currencyId: number;
-	currencyDetails: CurrencyDetails;
-	currencyQuantityPurchased: number;
-	currencyData: number[][];
-	currancyOHLCData: OCLHRawDdata[];
-	loading = true;
-	affordableBuy: boolean;
-	affordableSell: boolean;
-	buyQuantity: number;
-	sellQuantity: number;
+    private loggedUser: User;
+    currencyId: number;
+    currencyDetails: CurrencyDetails;
+    currencyQuantityPurchased: number;
+    currencyData: number[][];
+    currancyOHLCData: OCLHRawDdata[];
+    loading = true;
+    affordableBuy: boolean;
+    affordableSell: boolean;
+    buyQuantity: number;
+    sellQuantity: number;
 
-	constructor(private detailsRoute: ActivatedRoute,
-		private currencyDetailsProcessor: CurrencyProcessorService,
-		private currencyProviderService: CurrencyProviderService,
-		private currencyTransactionsService: CurrencyTransactionsService,
-		private userService: UserService,
-		public toastr: ToastsManager,
-		public toastrService: ToastrService,
-		private vRef: ViewContainerRef,
-		private userAuthService: UserAuthService) {
-		this.toastr.setRootViewContainerRef(this.vRef);
-	}
+    constructor(private detailsRoute: ActivatedRoute,
+        private currencyDetailsProcessor: CurrencyProcessorService,
+        private currencyProviderService: CurrencyProviderService,
+        private currencyTransactionsService: CurrencyTransactionsService,
+        private userService: UserService,
+        public toastr: ToastsManager,
+        public toastrService: ToastrService,
+        private vRef: ViewContainerRef,
+        private userAuthService: UserAuthService) {
+        this.toastr.setRootViewContainerRef(this.vRef);
+    }
 
-	ngOnInit() {
-		this.detailsRoute.params.subscribe((params) => {
-			this.currencyId = params.id;
-			this.userService.getUserDetails()
-				.map((res) => res.json())
-				.subscribe((response) => {
-					this.loggedUser = response.user;
-					this.loadCurrencyDetails();
-				});
-		});
-	}
-	private loadCurrencyDetails() {
-		this.currencyDetailsProcessor.getFullCurrencyDetails(this.currencyId)
-			.subscribe((result) => {
-				this.currencyDetails = result;
-				this.currencyProviderService.getCoinIOHLCInformation(this.currencyDetails.symbol)
-					.map((res) => res.json())
-					.map((res) => res.result.Data.filter(x => x.close > 0))
-					.do((res) => this.currancyOHLCData = res)
-					.map((res) => Object.values(res))
-					.map((res) => res.map(x => [x.time * 1000, x.close]))
-					.subscribe((response) => {
-						this.loading = false;
-						this.currencyData = (response);
-						if (!this.loggedUser) {
-							return this.currencyQuantityPurchased = 0;
-						} else if (!this.loggedUser.currencies[this.currencyDetails.symbol]) {
-							return 0;
-						}
-						this.currencyQuantityPurchased = this.loggedUser.currencies[this.currencyDetails.symbol].quantity;
-					});
-			});
-	}
-	isLogged() {
-		return this.userAuthService.isLogged();
-	}
-	private updateUser() {
-		this.userService.getUserDetails()
-			.map((res) => res.json())
-			.subscribe((response) => {
-				this.loggedUser = response.user;
-				this.currencyQuantityPurchased = this.loggedUser.currencies[this.currencyDetails.symbol].quantity;
-			});
-	}
-	isBuyAffordable(value: number) {
-		this.affordableBuy = this.loggedUser.balance >=
-			value * this.currencyDetails.priceConversions['USD'];
-	}
-	isSellAfordable(value: number) {
-		if (!this.loggedUser.currencies[this.currencyDetails.symbol]) {
-			this.affordableSell = false;
-			return;
-		}
+    ngOnInit() {
+        this.detailsRoute.params.subscribe((params) => {
+            this.currencyId = params.id;
+            this.userService.getUserDetails()
+                .map((res) => res.json())
+                .subscribe((response) => {
+                    this.loggedUser = response.user;
+                    this.loadCurrencyDetails();
+                });
+        });
+    }
+    private loadCurrencyDetails() {
+        this.currencyDetailsProcessor.getFullCurrencyDetails(this.currencyId)
+            .subscribe((result) => {
+                this.currencyDetails = result;
+                this.currencyProviderService.getCoinIOHLCInformation(this.currencyDetails.symbol)
+                    .map((res) => res.json())
+                    .map((res) => res.result.Data.filter((x: { close: number }) => x.close > 0))
+                    .do((res) => this.currancyOHLCData = res)
+                    .map((res) => Object.values(res))
+                    .map((res) => res.map((x: { close: any, time: number }) => [x.time * 1000, x.close]))
+                    .subscribe((response) => {
+                        this.loading = false;
+                        this.currencyData = (response);
+                        if (!this.loggedUser) {
+                            return this.currencyQuantityPurchased = 0;
+                        } else if (!this.loggedUser.currencies[this.currencyDetails.symbol]) {
+                            return 0;
+                        }
+                        this.currencyQuantityPurchased = this.loggedUser.currencies[this.currencyDetails.symbol].quantity;
+                    });
+            });
+    }
+    isLogged() {
+        return this.userAuthService.isLogged();
+    }
+    private updateUser() {
+        this.userService.getUserDetails()
+            .map((res) => res.json())
+            .subscribe((response) => {
+                this.loggedUser = response.user;
+                this.currencyQuantityPurchased = this.loggedUser.currencies[this.currencyDetails.symbol].quantity;
+            });
+    }
+    isBuyAffordable(value: number) {
+        this.affordableBuy = this.loggedUser.balance >=
+            value * this.currencyDetails.priceConversions['USD'];
+    }
+    isSellAfordable(value: number) {
+        if (!this.loggedUser.currencies[this.currencyDetails.symbol]) {
+            this.affordableSell = false;
+            return;
+        }
 
-		this.affordableSell = this.loggedUser.currencies[this.currencyDetails.symbol].quantity >=
-			value;
-	}
-	getUserBalance() {
-		return this.loggedUser.balance;
-	}
-	onBuy() {
-		this.currencyTransactionsService.buyCurrency(this.currencyId, this.currencyDetails.symbol,
-			this.currencyDetails.priceConversions['USD'], this.buyQuantity)
-			.subscribe((r) => {
-				this.updateUser();
-				const boughtValueUSD = this.currencyDetails.priceConversions['USD'] * this.buyQuantity;
-				this.loggedUser.balance -= boughtValueUSD;
-				this.toastrService.showSuccess(`You bought ${this.buyQuantity} ${this.currencyDetails.symbol} for ${boughtValueUSD}$`);
-				this.buyQuantity = null;
-				this.affordableBuy = false;
-			});
-	}
-	onSell() {
-		this.currencyTransactionsService.sellCurrency(this.currencyId, this.currencyDetails.symbol,
-			this.currencyDetails.priceConversions['USD'], this.sellQuantity)
-			.subscribe((r) => {
-				this.updateUser();
-				const soldValueUSD = this.currencyDetails.priceConversions['USD'] * this.sellQuantity;
-				this.loggedUser.balance += soldValueUSD;
-				this.toastrService.showSuccess(`You sold ${this.sellQuantity} ${this.currencyDetails.symbol} for ${soldValueUSD}$`);
-				this.sellQuantity = null;
-				this.affordableSell = false;
-			});
-	}
+        this.affordableSell = this.loggedUser.currencies[this.currencyDetails.symbol].quantity >=
+            value;
+    }
+    getUserBalance() {
+        return this.loggedUser.balance;
+    }
+    onBuy() {
+        this.currencyTransactionsService.buyCurrency(this.currencyId, this.currencyDetails.symbol,
+            this.currencyDetails.priceConversions['USD'], this.buyQuantity)
+            .subscribe((r) => {
+                this.updateUser();
+                const boughtValueUSD = this.currencyDetails.priceConversions['USD'] * this.buyQuantity;
+                this.loggedUser.balance -= boughtValueUSD;
+                this.toastrService.showSuccess(`You bought ${this.buyQuantity} ${this.currencyDetails.symbol} for ${boughtValueUSD}$`);
+                this.buyQuantity = null;
+                this.affordableBuy = false;
+            });
+    }
+    onSell() {
+        this.currencyTransactionsService.sellCurrency(this.currencyId, this.currencyDetails.symbol,
+            this.currencyDetails.priceConversions['USD'], this.sellQuantity)
+            .subscribe((r) => {
+                this.updateUser();
+                const soldValueUSD = this.currencyDetails.priceConversions['USD'] * this.sellQuantity;
+                this.loggedUser.balance += soldValueUSD;
+                this.toastrService.showSuccess(`You sold ${this.sellQuantity} ${this.currencyDetails.symbol} for ${soldValueUSD}$`);
+                this.sellQuantity = null;
+                this.affordableSell = false;
+            });
+    }
 }
--- src/app/currency/currency-listing/currency-listing.component.ts
+++ src/app/currency/currency-listing/currency-listing.component.ts
@@ -5,64 +5,64 @@ import 'rxjs/Rx';
 import { DatatableComponent } from '@swimlane/ngx-datatable';
 
 @Component({
-  selector: 'app-currency-listing',
-  templateUrl: './currency-listing.component.html',
-  styleUrls: ['./currency-listing.component.css']
+    selector: 'app-currency-listing',
+    templateUrl: './currency-listing.component.html',
+    styleUrls: ['./currency-listing.component.css']
 })
 export class CurrencyListingComponent implements OnInit {
 
-  @ViewChild(DatatableComponent) table: DatatableComponent;
-  @ViewChild('editTmpl') editTmpl: TemplateRef<any>;
-  rows = [];
-  selected = [];
-  columns = [];
-  loadedItems = [];
-  loading = true;
+    @ViewChild(DatatableComponent) table: DatatableComponent;
+    @ViewChild('editTmpl') editTmpl: TemplateRef<any>;
+    rows = [];
+    selected = [];
+    columns = [];
+    loadedItems = [];
+    loading = true;
 
-  constructor(private currencyProviderService: CurrencyProviderService, private router: Router) { }
+    constructor(private currencyProviderService: CurrencyProviderService, private router: Router) { }
 
-  ngOnInit() {
-    this.columns = [{
-      cellTemplate: this.editTmpl,
-      name: 'Logo'
-    },
-    { prop: 'Name' },
-    { prop: 'CoinName' },
-    { prop: 'Algorithm' },
-    ];
-    this.currencyProviderService.getAllCoinInformation()
-      .map((res) => res.json())
-      .map((res) => Object.values(res.results.Data))
-      .map((res) => res.filter(x => x.Name.match('.*[A-Z].*')))
-      .subscribe((response) => {
+    ngOnInit() {
+        this.columns = [{
+            cellTemplate: this.editTmpl,
+            name: 'Logo'
+        },
+        { prop: 'Name' },
+        { prop: 'CoinName' },
+        { prop: 'Algorithm' },
+        ];
+        this.currencyProviderService.getAllCoinInformation()
+            .map((res) => res.json())
+            .map((res) => Object.values(res.results.Data))
+            .map((res) => res.filter((x: { Name: string }) => x.Name.match('.*[A-Z].*')))
+            .subscribe((response) => {
 
-        this.loadedItems = response;
-        this.rows = this.loadedItems;
-        this.loading = false;
-      });
-  }
+                this.loadedItems = response;
+                this.rows = this.loadedItems;
+                this.loading = false;
+            });
+    }
 
-  updateFilter(event) {
-    const val = event.target.value.toLowerCase();
-    // filter our data
-    const temp = this.loadedItems.filter(d => {
-      return d.FullName.toLowerCase().indexOf(val) !== -1 || !val;
-    });
+    updateFilter(event) {
+        const val = event.target.value.toLowerCase();
+        // filter our data
+        const temp = this.loadedItems.filter(d => {
+            return d.FullName.toLowerCase().indexOf(val) !== -1 || !val;
+        });
 
-    // update the rows
-    this.rows = temp;
-    // // Whenever the filter changes, always go back to the first page
-    this.table.offset = 0;
-  }
+        // update the rows
+        this.rows = temp;
+        // // Whenever the filter changes, always go back to the first page
+        this.table.offset = 0;
+    }
 
-  onSelect(selected) {
-    const currencyUrl = selected.selected[0].Id;
-    console.log(currencyUrl);
-    this.router.navigate(['/currency/' + currencyUrl]);
-  }
+    onSelect(selected: { selected: { Id: [string | number] } }) {
+        const currencyUrl = selected.selected[0].Id;
+        console.log(currencyUrl);
+        this.router.navigate(['/currency/' + currencyUrl]);
+    }
 
-  onActivate(event) {
-    // console.log('Activate Event', event);
-  }
+    onActivate(event) {
+        // console.log('Activate Event', event);
+    }
 
 }
