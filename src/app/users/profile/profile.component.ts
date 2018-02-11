3c5c92748fd2a3c78fe9c505e6c53c8c6e0cb82c
grozdanovgg <grozdanovgg@gmail.com>
2018-02-11 23:25:11 +0200

    currency listing html template missing div

--- src/app/users/profile/profile.component.ts
+++ src/app/users/profile/profile.component.ts
@@ -9,86 +9,86 @@ import { User } from './../../models/user.model';
 import { UserService } from './../../services/user.service';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+	selector: 'app-profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit, OnChanges {
 
-  public userForm: FormGroup;
-  public disabled: string | boolean;
-  user: User;
-  original: User;
-  loaded: boolean;
-  editing: boolean;
-  userCurrencies: Array<{}>;
+	public userForm: FormGroup;
+	public disabled: string | boolean;
+	user: User;
+	original: User;
+	loaded: boolean;
+	editing: boolean;
+	userCurrencies: Array<{}>;
 
-  constructor(
-    private router: Router,
-    private userService: UserService,
-    private userAuthService: UserAuthService,
-    private formValidationService: FormValidationService,
-  ) { }
+	constructor(
+		private router: Router,
+		private userService: UserService,
+		private userAuthService: UserAuthService,
+		private formValidationService: FormValidationService,
+	) { }
 
-  ngOnInit() {
-    if (!this.userAuthService.isLogged()) {
-      this.router.navigateByUrl('');
-      return;
-    }
+	ngOnInit() {
+		if (!this.userAuthService.isLogged()) {
+			this.router.navigateByUrl('');
+			return;
+		}
 
-    this.userForm = this.formValidationService.formValidation(null, 'firstName', 'lastName', 'email', null, null);
-    this.userForm.statusChanges.subscribe(data => {
+		this.userForm = this.formValidationService.formValidation(null, 'firstName', 'lastName', 'email', null, null);
+		this.userForm.statusChanges.subscribe(data => {
 
-      this.formValidationService.submitButtonValidation(this.userForm);
-      if (this.userForm['FormIsOK']) {
-        this.disabled = null;
-      } else {
-        this.disabled = 'disabled';
-      }
-    });
+			this.formValidationService.submitButtonValidation(this.userForm);
+			if (this.userForm['FormIsOK']) {
+				this.disabled = null;
+			} else {
+				this.disabled = 'disabled';
+			}
+		});
 
-    Observable.zip(this.userService.getUserDetails(),
-      this.userService.getUserCurrencies())
-      .map((response) => {
-        return {
-          details: response[0].json(),
-          curencies: response[1].json()
-        };
-      })
-      .subscribe((response) => {
-        this.user = response.details.user;
-        this.userCurrencies = response.curencies.currencies;
-        this.loaded = true;
-        setTimeout(() => this.editing = false, 1);
-      },
-      error => console.log('Error - ' + error)
-      );
-  }
+		Observable.zip(this.userService.getUserDetails(),
+			this.userService.getUserCurrencies())
+			.map((response) => {
+				return {
+					details: response[0].json(),
+					curencies: response[1].json()
+				};
+			})
+			.subscribe((response) => {
+				this.user = response.details.user;
+				this.userCurrencies = response.curencies.currencies;
+				this.loaded = true;
+				setTimeout(() => this.editing = false, 1);
+			},
+			error => console.log('Error - ' + error)
+			);
+	}
 
-  ngOnChanges(changes: SimpleChanges): void {
+	ngOnChanges(changes: SimpleChanges): void {
 
-  }
+	}
 
-  makeEditable() {
-    this.original = <User>JSON.parse(JSON.stringify(this.user));
-    this.editing = true;
-  }
+	makeEditable() {
+		this.original = <User>JSON.parse(JSON.stringify(this.user));
+		this.editing = true;
+	}
 
-  cancelUpdate() {
-    this.editing = false;
-    this.user = this.original;
-  }
+	cancelUpdate() {
+		this.editing = false;
+		this.user = this.original;
+	}
 
-  updateProfile() {
-    this.userService.updateUserDetails(this.user)
-      .map((res) => res.json())
-      .subscribe(
-      (response) => {
-        this.editing = false;
-      },
-      (error) => {
-        console.log('Error - ' + error);
-      }
-      );
-  }
+	updateProfile() {
+		this.userService.updateUserDetails(this.user)
+			.map((res) => res.json())
+			.subscribe(
+			(response) => {
+				this.editing = false;
+			},
+			(error) => {
+				console.log('Error - ' + error);
+			}
+			);
+	}
 }
